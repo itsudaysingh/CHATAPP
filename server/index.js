7cb0af6cfc60a957fc3d8f863e0ae0334a754cf3
Uday Singh <uday@acpl.co.in>
2020-07-05 11:32:22 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT =  process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection',(socket)=>{
+  console.log('we have new connection !!!');
+
+  socket.on('disconnection',()=>{
+    console.log('User had left');
+  });
+
+});
+
+app.use(router);
+
+server.listen(PORT, ()=>console.log(`Server Has Started on ${PORT}`));
+
